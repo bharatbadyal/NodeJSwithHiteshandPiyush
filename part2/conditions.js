8d6966326941b4012d701bf961a64151b3586bc9
Bharat <myprof219@gmail.com>
2025-08-21 20:25:59 +0530

    Conditonals

---
+++ part2/conditions.js
@@ -0,0 +1,37 @@
+// Checking ir a number is greate than another number:
+
+let num1 = 5;
+let num2 = 8;
+
+if(num1 > num2){
+    console.log("Num 1 is greater then num 2")
+}else{
+    console.log("Num 2 is greater then num 1")
+}
+
+// Check is a string is equal to another string:
+
+let username_ = "chai";
+let anotherUserName = "chai";
+
+if(username_ == anotherUserName){
+    console.log("Pick another username...")
+}
+
+// Checking if a variable is a number or not:
+
+let score = 44;
+
+if(typeof score === "number"){
+    console.log("This is number")
+}
+
+// Checking if a boolean value is true or false:
+
+let bool = false;
+
+if(bool){
+    console.log("The value is True!")
+}else{
+    console.log("The value is False!")
+}
